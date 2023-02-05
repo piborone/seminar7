f3a4639beb3f7393f76276b69f507ed923e23d8b
Pavel B <boronenkov999@mail.ru>
2023-02-05 22:47:42 +0300

    прописаны методы в 47

--- zadacha47/Program.cs
+++ zadacha47/Program.cs
@@ -3,26 +3,61 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
-Console.WriteLine("Введите число M");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число N");
-int n = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine();
-Random rand = new Random();
-double[,] array2d = new double[m, n];
-for(int i = 0; i < m; i++ )
+int InputNumbers(string str)
 {
-    for(int j = 0; j < n; j++)
+    int numbers;
+    string text;
+    while(true)
     {
-       array2d[i, j] = rand.Next(-10, 11)+rand.NextDouble();
+        Console.WriteLine(str);
+        text = Console.ReadLine();
+        if(int.TryParse(text, out numbers))
+        {
+            while(numbers < 1)
+            {
+            System.Console.WriteLine("Размер массива не может иметь отрицательное или нулевое значение");
+            System.Console.WriteLine("Введите число повторно: ");
+            numbers = Convert.ToInt32(Console.ReadLine());
+            }
+            break;
+        }
+        else
+        {
+        Console.WriteLine("Ошибка!!! Введено не верное значение!");
+        }
     }
-    
+    return numbers;
 }
-for(int i = 0; i < array2d.GetLength(0); i++)
+
+void PrintMatrix(double[,] matr) // Вывод массива
 {
-    for(int j = 0; j < array2d.GetLength(1); j++)
+for(int i = 0; i < matr.GetLength(0); i++)
+{
+    for(int j = 0; j < matr.GetLength(1); j++)
     {
-        System.Console.Write(Math.Round(array2d[i,j],2) + $"\t");
+        System.Console.Write(Math.Round(matr[i,j],2) + $"\t"); // округление переменных до 2х знаков после запятой
     }
     System.Console.WriteLine();
 }
+}
+
+double[,] FillMatrix(int rows, int cols) // рандомное заполнение массива
+{
+    Random rand = new Random();
+    double[,] array2d = new double[rows, cols];
+    for(int i = 0; i < rows; i++ )
+    {
+        for(int j = 0; j < cols; j++)
+        {
+       array2d[i, j] = rand.Next(-10, 11)+rand.NextDouble(); // вещественные числа
+        }
+    }
+
+return array2d;
+}
+
+int rows = InputNumbers("Введите число M: ");
+int cols = InputNumbers("Введите число N: ");
+System.Console.WriteLine();
+double[,] array2d = FillMatrix(rows, cols);
+PrintMatrix(array2d);
